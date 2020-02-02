9f2284bb75358f934b70809afb39e4c2c466fcbe
Amir <onikoyiamir@yahoo.com>
2020-02-02 22:52:39 +0100

    Setting up ionic tools

---
+++ src/pages/Tab2.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+// import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg, IonActionSheet } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons';
+import { usePhotoGallery} from '../hooks/usePhotoGallery';
+
+const Tab2: React.FC = () => {
+  const { takePhoto } = usePhotoGallery();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
